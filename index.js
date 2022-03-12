c745e0dd36b8ed7c7a56590d7fbd61aacb9fcd0a
Matt Loar <matt@loar.name>
2022-03-11 19:04:47 -0600

    Adjust heat map

--- index.js
+++ index.js
@@ -30,14 +30,23 @@ features.forEach((f, i) => features.slice(i + 1).forEach(g => { f.geo = f.geo.di
 const uniqueRates = Array.from(new Set(features.map(f => f.taxrate)));
 const maxRate = Math.max(...uniqueRates);
 const minRate = Math.min(...uniqueRates);
-const color1 = {red: 0, green: 0, blue: 255};
-const color2 = {red: 255, green: 0, blue: 0};
-const resultRed = percent => color1.red + percent * (color2.red - color1.red);
-const resultGreen = percent => color1.green + percent * (color2.green - color1.green);
-const resultBlue = percent => color1.blue + percent * (color2.blue - color1.blue);
-const hexNum = num => num < 16 ? "0" + Math.round(num).toString(16) : Math.round(num).toString(16);
-const spread = rate => (rate - minRate) / (maxRate - minRate);
-const computeColor = rate => `88${hexNum(resultBlue(spread(rate)))}${hexNum(resultGreen(spread(rate)))}${hexNum(resultRed(spread(rate)))}`;
+
+function computeColor(rate) {
+  const value = (rate - minRate) / (maxRate - minRate);
+  const colors = [
+    {red: 0, green: 0, blue: 255},
+    {red: 0, green: 255, blue: 0},
+    {red: 255, green: 255, blue: 0},
+    {red: 255, green: 0, blue: 0}
+  ];
+
+  const idx1 = value <= 0 ? 0 : value >= 1 ? colors.length - 1 : Math.floor(value * (colors.length - 1));
+  const idx2 = value <= 0 ? 0 : value >= 1 ? colors.length - 1 : idx1 + 1;
+  const fract = value - idx1;
+
+  const hexNum = num => num < 16 ? "0" + Math.round(num).toString(16) : Math.round(num).toString(16);
+  return `88${hexNum((colors[idx2].blue - colors[idx1].blue)*fract + + colors[idx1].blue)}${hexNum((colors[idx2].green - colors[idx1].green)*fract + colors[idx1].green)}${hexNum((colors[idx2].red - colors[idx1].red) * fract + colors[idx1].red)}`;
+}
 
 const chunks = Math.ceil(features.length / 2000);
 
